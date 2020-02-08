1cb97d003655054557363b3a3dc67c0ff2898eed
Aleksei <supportalex@rambler.ru>
2020-02-08 14:09:30 +0300

    Initial Commit

---
+++ HW2.4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.4
+//
+//  Created by Алексей Маслобоев on 08.02.2020.
+//  Copyright © 2020 Алексей Маслобоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
